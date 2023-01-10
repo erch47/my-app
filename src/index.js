abef626e3c9cd140d3279fbbab13b2da399c039f
erch47 <erik.chr1@hotmail.se>
2023-01-10 21:31:14 +0100

    Finished demorouter

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import DataTable from "./DataTable";
+import DemoRouter from './DemoRouter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <DataTable />
+    <DemoRouter />
 
 );
